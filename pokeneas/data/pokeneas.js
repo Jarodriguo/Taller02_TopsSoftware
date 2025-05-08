3e9116a8270d375f196b1485327a57ff19357c67
Sebastián Cano Rincón <sebastiancano21@hotmail.es>
2025-05-07 20:07:55 -0500

    📷 uploadad the images property, changed routes, added views and 🫏added 4 more pokeneas

--- pokeneas/data/pokeneas.js
+++ pokeneas/data/pokeneas.js
@@ -5,6 +5,7 @@ const pokeneas = [
         nombre: "Gatman",
         poder: "Invisibilidad",
         altura: 0.5,
+        imagen: "https://pakeneas-ing-software-2025.s3.us-east-1.amazonaws.com/gat.jpg",
         frase: "Jugar con los ratones es esencial para desarrollar el poderío gatuno"
     },
     {
@@ -12,6 +13,7 @@ const pokeneas = [
         nombre: "Superro",
         poder: "Super fuerza",
         altura: 1.8,
+        imagen: "https://pakeneas-ing-software-2025.s3.us-east-1.amazonaws.com/superPerro.jpg",
         frase: "Si fuerza quieres tener, muy buen cuido debes comer"
     },
     {
@@ -19,7 +21,40 @@ const pokeneas = [
         nombre: "Ratonito",
         poder: "Super velocidad",
         altura: 0.9,
+        imagen: "https://pakeneas-ing-software-2025.s3.us-east-1.amazonaws.com/ratonito.jpg",
         frase: "No hay actividad que de más velocidad que usar la rueda"
+    },
+    {
+        id: 4,
+        nombre: "Marijuano",
+        poder: "La bareta",
+        altura: 2.0,
+        imagen: "https://pakeneas-ing-software-2025.s3.us-east-1.amazonaws.com/Marijuano.jpg",
+        frase: "Ey socio, inviteme a un puntico"
+    },
+    {
+        id: 5,
+        nombre: "Enviguaro",
+        poder: "Fumar",
+        altura: 1.4,
+        imagen: "https://pakeneas-ing-software-2025.s3.us-east-1.amazonaws.com/Enviguaro.jpg",
+        frase: "¿Dónde está mi vaporizador?"
+    },
+    {
+        id: 6,
+        nombre: "Pichurria",
+        poder: "Visajosería",
+        altura: 0.3,
+        imagen: "https://pakeneas-ing-software-2025.s3.us-east-1.amazonaws.com/Pichurria.jpg",
+        frase: "Junior, gonorrea se lo juro que se vinieron todas las chimbas de Medellín"
+    },
+    {
+        id: 7,
+        nombre: "Carabana",
+        poder: "Fuerza Mental",
+        altura: 0.9,
+        imagen: "https://pakeneas-ing-software-2025.s3.us-east-1.amazonaws.com/Carabana.jpg",
+        frase: "La capital de Bangladesh? No sé pai, no nea estoy grochis porque tuki tuki muñeco al altis, como yo no salgo de aqui de Medellín, Itagui"
     }
 ]
 
--- pokeneas/routes/apiRoute.js
+++ pokeneas/routes/apiRoute.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const os = require('os'); // Módulo que se usa para obtener el ID del contenedor
+const os = require('os');
 const pokeneas = require('../data/pokeneas.js')
+
 
 router.get('/pokenea', (req, res) => {
     const pokenea = pokeneas[Math.floor(Math.random() * pokeneas.length)];
@@ -13,5 +14,14 @@ router.get('/pokenea', (req, res) => {
         contenedor: os.hostname()
     });
 });
+
+router.get('/pokenea/display', (req, res) => {
+    const pokenea = pokeneas[Math.floor(Math.random() * pokeneas.length)];
+    const containerID = os.hostname()
+    res.render('pokeneaDisplay', {
+        pokenea: pokenea,
+        contenedor: containerID
+    })
+})
 
 module.exports = router
